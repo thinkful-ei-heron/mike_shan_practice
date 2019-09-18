87a3a8403ff9a038bfd2fc5abf5b0fdceab816da
Michael Kirsch <michaelskirsch@gmail.com>
2019-09-18 10:09:32 -0600

    object update method

--- practice_drills.js
+++ practice_drills.js
@@ -12,3 +12,11 @@ function createMyObject() {
 }
 
 console.log(createMyObject());
+
+function updateObject(obj) {
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
+}
